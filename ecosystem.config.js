f92aceea8cdec85d71db6b124b4ab984749ce950
mehidy <mehidy.gb@gmail.com>
2025-08-13 22:43:22 +0600

    feat: add ecosystem configuration for production deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [
+        {
+            name: "wii-edu",
+            script: "node_modules/next/dist/bin/next",
+            args: "start -p 50000",
+            cwd: "./", // path to your Next.js project root
+            instances: 1, // you can change to "max" for cluster mode
+            exec_mode: "fork", // use "cluster" for multiple instances
+            watch: false, // disable watch in production
+            env: {
+                NODE_ENV: "production",
+                PORT: 50007
+            }
+        }
+    ]
+};
